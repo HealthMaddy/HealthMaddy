c1588ad00aee8618827c52e4d27dbb9ca90e8086
raunak_io <raunaksrivastava@ninjacart.com>
2024-05-21 13:30:59 +0530

    firebase rule changes and meta tags addition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { Meta } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
- 
+export class AppComponent implements OnInit {
+  constructor(private metaService: Meta) {}
+
+  ngOnInit(): void {
+    this.metaService.addTags([
+      {
+        name: "keywords",
+        content: `Health Maddy, HEALTH MADDY , health maddy ,health, maddy, maddy health ,   best ,  pre workout , spell pre workout , onlone fitness classes ,  protein , best protein ,  fat loss, muscle gain , lean , gain , muscule, power , pump , amino acids , for weight loss , at home  , Best pre workout meal,
+        Best Pre workout in India,
+        Best pre workout at home,
+        Best pre workout natural,
+        Best Pre workout for men,
+        Top 10 Pre workout supplement,
+        Muscle gain best Pre workout,
+        Best Pre workout under 1000,
+        Whey protein , Oats , peanut butter, genuine , authentic products, 
+`,
+      },
+      { name: "author", content: "Raunak Srivastava, Abhishek Srivastava" },
+      { name: "robots", content: "index, home" },
+
+      { charset: "UTF-8" },
+    ]);
+  }
 }
